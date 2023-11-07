fef63a6e41ddde6766577a5a7321deb4a224f7b5
Nitika Saxena <97978599+nitika-saxena@users.noreply.github.com>
2023-11-07 19:50:13 +0530

    Create BracketBalancing.java

---
+++ Lab3_DSA_Question1/src/com/greatLearning/question1/BracketBalancing.java
@@ -0,0 +1,42 @@
+package com.greatLearning.question1;
+
+import java.util.Stack;
+
+public class BracketBalancing {
+	
+	public  boolean isBalancedBracket(String input)
+	{
+		Stack<Character> stack = new Stack<Character>();
+		for (char ch : input.toCharArray()) {
+			if (ch ==  '{'|| ch == '(' || ch == '[') {
+				stack.push(ch);
+				continue;
+			}
+
+			if(stack.isEmpty()) { 
+				return false;
+			}
+			
+      switch (ch) {
+			case '}':
+				if(stack.pop() != '{') {
+				  return false;
+				}
+				break;
+			case ']':
+				if(stack.pop() != '[') {
+					return false;
+				}
+				break;	
+			case ')':
+				if(stack.pop() != '(') {
+					return false;
+				}
+				break;	
+			default:	
+				return false;
+			}
+		}				
+		return (stack.isEmpty());
+	}
+}
