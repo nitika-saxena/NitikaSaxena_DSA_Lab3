8d37e6f09a725db1463baaf70841c4054a4d4ed4
Nitika Saxena <97978599+nitika-saxena@users.noreply.github.com>
2023-11-07 19:48:57 +0530

    Create Driver.java

---
+++ Lab3_DSA_Question1/src/com/greatLearning/question1/Driver.java
@@ -0,0 +1,31 @@
+package com.greatLearning.question1;
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		BracketBalancing bb = new BracketBalancing ();
+
+		System.out.println("Please enter a string of brackets:");
+		Scanner sc  = new Scanner(System.in);
+
+		String input = sc.nextLine();
+
+		sc.close();
+		
+		//System.out.println(input);
+		
+		if (input == null || input.isEmpty()) { 
+			System.out.println("The entered String is empty");
+		}
+
+		else if (bb.isBalancedBracket(input)) {
+			System.out.println("The entered string has Balanced Brackets");
+
+		} else { 
+			System.out.println("The entered Strings do not contain Balanced Brackets");
+    }
+  }
+}
+	
